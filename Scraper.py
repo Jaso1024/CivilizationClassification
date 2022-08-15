983b337275452c02e019e13f8f6e847ccd4fe689
Jason Abohwo <jabohwho@gmail.com>
2022-08-14 20:15:56 -0500

    Added phrase removal

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 class Scraper:
     def __init__(self) -> None:
@@ -9,8 +10,44 @@ class Scraper:
         self.empires = []
 
     def scrape_training(self):
+        for empire in self.urls["Training"]:
+            urls = self.urls["Training"][empire]
+            for url in urls:
+                request = requests.get(url)
+                soup = BeautifulSoup(request.content, "html.parser")
+                content = ""
+                for tag in soup.find_all('p'):
+                    text = tag.get_text()
+                    if empire.lower() in text.lower():
+                        content += text
+
+                content = self.format_data(content)
+
+
+                return content
+
+
+    def scrape_testing(self):
         pass
-    
+
+    def remove_phrases(self, content):
+        content = content.lower()
+        with open("Resources/Data/Removalphrases.txt", "r") as file:
+            file = [line.strip().replace("\n", "") for line in file.readlines()]
+            for line in file:
+                print(line, line.lower() in content)
+                content = re.sub(line.lower(), " ", content)
+        return content
+        
+    def format_data(self, content):
+        content = re.sub("  +", " ", content)
+        content = re.sub("\n", " ", content)
+        content = self.remove_phrases(content)
+        return content
+
+    def to_csv(self, filename):
+        pass
+
     def set_empires(self):
         with open("Resources/Data/URLs.txt", "r") as file:
             empires = file.readline().split(" ")
@@ -37,9 +74,7 @@ class Scraper:
                 elif line in self.empires:
                     current_empire = line
                 elif self.is_link(line):
-                    self.urls[current_dataset][current_empire].append(line)
-                
-                                 
+                    self.urls[current_dataset][current_empire].append(line)                       
             
     def is_link(self, link):
         if link[:4] == 'http':
@@ -47,13 +82,4 @@ class Scraper:
                 return True
         return False
 
-
-    def scrape_testing(self):
-        pass
-
-    def format_data(self):
-        pass
-
-    def to_csv(self, filename):
-        pass
 
